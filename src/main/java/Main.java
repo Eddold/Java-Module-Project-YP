eec4723fe3394dfb9090b8a5ac3e3360ce45fbac
Натиг <nate@MacBook-Air-Natig.local>
2026-08-23 23:13:30 +0400

    Выполнил проектную работу

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,12 @@
-
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Generics project");
+    }
+}яpublic class Main {
+    public static void main(String[] args) {
+        System.out.println("Generics project");
+    }
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
